8ce03311c7b94d4dcb9404049cfd82655f7c67a4
pollosatyi <pollosatyi@mail.ru>
2025-02-16 00:04:15 +0300

    Добавьте файлы проекта.

---
+++ LoggerSolution3/Logger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerSolution3
+{
+    internal class Logger
+    {
+        public string Path { get; set; }
+        public DateTime dateTime { get; set; }
+
+        public Logger(string path)
+        {
+            Path = path;
+        }
+
+        public void Log()
+        {
+
+        }
+
+        public bool IsCheckFolder()
+        {
+            return Directory.Exists(Path);
+        }
+
+
+
+        public void CreateFolder()
+        {
+            Directory.CreateDirectory(Path);
+        }
+
+        private void CreateFile()
+        {
+            using (var streamWriter = new StreamWriter(Path, true))
+            {
+                streamWriter.Write("");
+            }
+        }
+    }
+}
---
+++ LoggerSolution3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LoggerSolution3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string path = @"../Test";
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Logger logger = new Logger(path);
+            
+        }
+    }
+}
